97d44df959b6cfe9d8cc3c043803e0016a3b6928
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-01 14:54:06 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,10 @@
+print("Hello python")
+Name=input("Enter the student name:")
+Roll_number=int(input('Enter the roll no:'))
+print('name of the student',Name)
+print("roll number:",Roll_number)
+CGPA=float(input("CGPA:"))
+percentage_mark=CGPA*10
+print("Name of the student",Name)
+print("Roll_nuber",Roll_number)
+
