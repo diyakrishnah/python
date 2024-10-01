96c07ac2251251d6b69b7b47a3be0d81cfdb2a07
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-01 15:26:42 +0530

    header added

---
+++ studentd.py
@@ -0,0 +1,15 @@
+'''
+Author:Diya Krishna
+Date:01-10-2024
+Version:1.0
+'''
+
+print("Hello python")
+Name=input("Enter the student name:")
+Roll_number=int(input('Enter the roll no:'))
+Roll_number=Roll_number+1
+CGPA=float(input("CGPA:"))
+percentage_mark=CGPA*10
+print("Name of the student:",Name)
+print("Roll_nuber",Roll_number)
+print("mark_percentage",percentage_mark,"%")
