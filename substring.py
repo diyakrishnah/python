77fd5eee459b6d964fbf6927c2311613de841513
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-08 15:10:14 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR:DIYA KRISHNA
+VERSION:1.1
+DATE:8/10/2024
+'''
+first_name=input("enter your  first name:")
+last_name=input("enter your last name:")
+full_name=first_name+last_name
+print(full_name)
+length=len(first_name)
+extracted_first_name=(full_name[:length])
+print(extracted_first_name)
