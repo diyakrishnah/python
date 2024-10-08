c76370fedd38e756cd56cc6cccf15018263eeee0
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-08 15:09:34 +0530

    Delete substring.py

--- substring.py
+++
@@ -1,7 +0,0 @@
-first_name=input("enter your  first name:")
-last_name=input("enter your last name:")
-full_name=first_name+last_name
-print(full_name)
-length=len(first_name)
-extracted_first_name=(full_name[:length])
-print(extracted_first_name)
