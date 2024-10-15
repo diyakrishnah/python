c1263eba8aa38fc293311384b4880922a8a3300e
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 15:46:09 +0530

    Add files via upload

---
+++ ZOO TICKET.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR:DIYA KRISHNA M
+DATE:15/10/2024
+VERSION:1.1
+'''
+age=int(input("enter you age:"))
+if age<10:
+    print("7 rupees")
+elif age>=10 and age<60:
+    print("10 rupees")
+else:
+    print("5 rupees")
