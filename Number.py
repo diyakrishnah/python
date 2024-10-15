c0ae4b8193801ade0a3ccdee9c47838362d12340
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:43 +0530

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,10 @@
+'''
+DATE:15/10/2024
+Python project to check the given number
+is positive or not
+ '''
+Number=int(input("Enter a number:"))
+if Number>0:
+    print("The given number",Number,"is positive.")
+else:
+    print("The given number",Number," is negative.")
