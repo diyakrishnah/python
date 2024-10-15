ddda9db9f948d8fa5424d125687db326dd58095b
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 15:59:03 +0530

    Add files via upload

---
+++ while loop1(odd numbers).py
@@ -0,0 +1,12 @@
+'''
+AUTHOR:DIYA KRISHNA
+Date:15/10/2024
+version:1.1
+'''
+limit=int(input("Enter a number:"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
