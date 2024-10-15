7d661f5b0cee164ed92adb4ef5e70b7cb38570a2
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 14:36:51 +0530

    Add files via upload

---
+++ AGE.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Diya Krishna.M
+DATE:15/10/2024
+VERSION:1.1
+'''
+age=int(input("Enter your age:"))
+if (age>=18):
+    print("Eligible for driving licence")
+else:
+    print("Not eligible for driving licence")
