ddef20df0bbc1438e56edd9541cc3ebbe4f43ddf
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 16:10:39 +0530

    Add files via upload

---
+++ odd number2.py
@@ -0,0 +1,11 @@
+'''
+AUTHOR:DIYA KRISHNA
+Date:15/10/2024
+version:1.1
+python program to generate upto n odd numbers
+'''
+limit=int(input("Enter a number:"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
