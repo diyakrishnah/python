7f84516946a90360f64b755973e1ad0612141f98
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-15 15:10:39 +0530

    Add files via upload

---
+++ largest of 2 numbers .py
@@ -0,0 +1,13 @@
+'''
+AUTHOR:DIYA KRISHNA M
+DATE:15/10/2024
+VERSION:1.1
+'''
+num1=int(input("Enter a number(a):"))
+num2=int(input("Enter a number(b)"))
+if num1>num2:
+    print(num1,"is largest")
+elif num2>num1:
+    print(num2,"is largest")
+else:
+    print("number(a)and number(b) are equal")
