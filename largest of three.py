65d615bfa172dc1378dadf6e41a1225409cb06bb
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-22 15:20:30 +0530

    Add files via upload

--- largest of three.py
+++ largest of three.py
@@ -8,7 +8,7 @@ b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
 if a>b and a>c:
     print(" The largest number is:",a)
-elif b>a and b>a:
+elif b>a and b>c:
     print("The largest number is:",b)
 else:
     print("The largest number is:",c)
