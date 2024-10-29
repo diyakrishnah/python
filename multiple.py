ba5b665774fc92a7dc446cc1c59c68839646f523
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-10-29 15:47:26 +0530

    Add files via upload

--- multiple.py
+++ multiple.py
@@ -1,4 +1,7 @@
+'''AUTHOR:Diyaa krishna
+versioj:1.1
+date:29/10/2024'''
 number=int(input("Enter a number:"))
-for j in range(1,13):
+for j in range(1,16):
     multiple=j*number
-    print (number,"*",j,"=",multiple)
+    print (number,"*",j,"\t =",multiple)
