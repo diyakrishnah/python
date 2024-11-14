83c71aa5dfdc43fef995f5f95d328103cc3858b5
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-11-14 21:05:11 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,48 @@
+i=[1,2,3,4,5]
+print(i)
+print(type(i))
+
+my_friend=['ozy','lux','siva']
+print(my_friend)
+
+employes=['lux',30,10000,66]
+print(employes)
+
+student=['lux','S1',[25,20,30]]
+print(student)
+print(student[2][2])
+
+even_number=list(range(2,20,2))
+
+for even_number in even_number:
+    print(even_number)
+for friend in my_friend:
+    print(friend)
+    print(len(friend))
+for friend in my_friend:
+    print("happy new year "+friend)
+
+a=[1,2,3,4,5]
+b=[6,7,8,9,10]
+c=[11,12,13,14,15]
+d=a+b
+print(a)
+print(d)
+
+binary=[2,4,6,8,10]
+sequence=binary*5
+print(sequence)
+
+even=[2,4,6]
+mul2=[2,4,6]
+seq=[1,2,3,]
+print(even==mul2)
+print(even==seq)
+
+prime=[2,3,5]
+print(prime>even)
+composite=[4,6,8]
+print(2 in even)
+    
+    
+    
