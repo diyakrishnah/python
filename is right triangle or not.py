623fc4cde6c1bc154bf706777f92f8413825cb82
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-11-30 10:09:10 +0530

    Add files via upload

---
+++ is right triangle or not.py
@@ -0,0 +1,13 @@
+first_side=int(input("Enter the length of the first side:"))
+second_side=int(input("Enter the length of the second side:"))
+third_side=int(input("Enter the length of the third side:"))
+def right_angled_triangle(first_side,second_side,third_side):
+    list=[first_side,second_side,third_side]
+    list.sort()
+    if list[2]**2==list[1]**2+list[0]**2:
+        return True
+    return False
+if right_angled_triangle(first_side,second_side,third_side):
+    print("The given sides are a part of a right angled triangle")
+else:
+    print("The given sides are not a part of a right angled triangle")
