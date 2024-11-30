fe6f3c1989ee8c757fcfee2c7602cf6c09706dea
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-11-30 10:56:28 +0530

    Add files via upload

---
+++ upper and lower case.py
@@ -0,0 +1,16 @@
+'''AUTHOR:Diya krishna
+version1.1
+date:30/11/2024'''
+def count_upper_lower_case(string):
+    for i in string:
+        upper_charector = 0
+        lower_charector = 0
+        if i.isupper():
+            upper_charector += 1
+        if i.islower():
+            lower_charector += 1
+    return upper_charector,lower_charector
+string=input("enter a string:")
+upper_charector,lower_charector=count_upper_lower_case(string)
+print(upper_charector)
+print(lower_charector)
