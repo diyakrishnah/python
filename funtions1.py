f9711a3ba136b7e036a86774ba996cbed1771048
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-11-30 09:21:30 +0530

    Add files via upload

---
+++ funtions1.py
@@ -0,0 +1,17 @@
+"""Author: Diya krishna
+version1.1
+Date:30/11/2024"""
+#adding 2 numbers
+def add_two_numbers():
+    number1=10
+    number2=20
+    sum=number1+number2
+    print(sum)
+add_two_numbers()
+#adding two numbers
+def adding_two_numbers(num1,num2):
+    sum=num1+num2
+    print(sum)
+adding_two_numbers(20,50)
+adding_two_numbers(27,68)
+adding_two_numbers(44,29)
